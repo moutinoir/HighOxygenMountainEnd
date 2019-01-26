5e84696267b2dd8057a528e86ff9b9ad6f71d1f1
Pauline Machabert <pauline@vitei.com>
2019-01-26 23:04:50 +0100

    point north show altitude

---
+++ Unity_HighOxygenMountainEnd/Assets/Scripts/PointNorth.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointNorth : MonoBehaviour
+{
+    void Update()
+    {
+        // Orient an object to point to magnetic north.
+        transform.rotation = Quaternion.Euler(0, -Input.compass.trueHeading, 0);
+    }
+}
