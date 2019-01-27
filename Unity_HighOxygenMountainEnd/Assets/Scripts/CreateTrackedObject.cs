f2685d6aada1fb9ad75b6545c365f19d23931b4e
Pauline Machabert <pauline@vitei.com>
2019-01-27 16:11:37 +0100

    nothing

---
+++ Unity_HighOxygenMountainEnd/Assets/Scripts/CreateTrackedObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTrackedObject : MonoBehaviour
+{
+    public GameObject trackedObject;
+    
+    IEnumerator PopTrackedObjects()
+    {
+        yield break;
+    }
+
+
+}
